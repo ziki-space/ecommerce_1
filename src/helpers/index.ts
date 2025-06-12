ee02447ff9682e59ddfc9716971eada75f4c0536
ziki-space <info@zikispace.com>
2025-06-12 01:45:05 -0700

    updates

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,7 +1,7 @@
 import { productData } from "@/constants/data";
 
 export const getProducts = async () => {
-  const res = await fetch("http://localhost:3000/api/products");
+  const res = await fetch("/api/products");
   if (!res.ok) {
     throw new Error("Faild to fetch products");
   }
