4858024da0a0ec635a4b01c0779b9df81a7cc9eb
ziki-space <info@zikispace.com>
2025-06-12 01:27:54 -0700

    updates

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse, NextRequest } from "next/server";
-import { Products } from "../../../../type";
+import { Products_ } from "../../../../type";
 import Stripe from "stripe";
 
 export const POST = async (request: NextRequest) => {
@@ -9,7 +9,7 @@ export const POST = async (request: NextRequest) => {
     const reqBody = await request.json();
     const { items, email } = await reqBody;
 
-    const extractingItems = await items.map((item: Products) => ({
+    const extractingItems = await items.map((item: Products_) => ({
       quantity: item.quantity,
       price_data: {
         currency: "usd",
